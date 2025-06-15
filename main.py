68292b4a8fc95fc4e717edd49cccebce57bd2e33
Joshua Feld <143060545+joshfeld2928@users.noreply.github.com>
2025-06-15 20:37:35 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fnn import microns
+from numpy import full, concatenate
+
+# load the model and neuron ids of the MICrONS scan 8-5
+model, ids = microns.scan(session=8, scan_idx=5)
+
+# example 3-second video (3 x 30 frames @ 30 FPS, 144 height, 256 width)
+frames = concatenate([
+    full(shape=[30, 144, 256], dtype="uint8", fill_value=0),   # 1 second of black
+    full(shape=[30, 144, 256], dtype="uint8", fill_value=128), # 1 second of gray
+    full(shape=[30, 144, 256], dtype="uint8", fill_value=255), # 1 second of white
+])
+
+# predict the response of neurons to the 3-second video
+print(response = model.predict(stimuli=frames))
